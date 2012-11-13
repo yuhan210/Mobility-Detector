bb5ef692482b5e73d122cf316c3e71e87d708a74
Tiffany Chen <yuhan210@gmail.com>
2012-11-13 13:11:41 -0500

    downsample.py updated

--- evaluation/downsample.py
+++ evaluation/downsample.py
@@ -1,18 +1,35 @@
 #! /usr/bin/python
 import sys
+import math
+
 if (len(sys.argv) < 3 )  :
    print "Usage : downsample.py trace-file sample-interval";
    exit(5);
+
 sensor_trace_file=open(sys.argv[1],"r");
 sampling_interval=float(sys.argv[2]);
-last_sample=-1.00
-for line in sensor_trace_file.readlines() :
+
+last_sample=-1.0
+content = sensor_trace_file.readlines()
+for i in range(len(content)) :
+ line = content[i]
  records=line.split(',')
- timestamp=float(records[1])/1.0e3  
+ timestamp=float(records[1])/1.0e3 
+ 
  if (last_sample==-1) :
     last_sample=timestamp
     print line.strip() 
- elif (timestamp-last_sample >= sampling_interval) :
-    last_sample=timestamp
-    print line.strip()
+ elif (timestamp-last_sample >= sampling_interval):
+    
+    prevLine = content[(i-1)]
+    prevRecords = prevLine.split(',')
+    prevTimeStamp = float(prevRecords[1])/1.0e3
+    
+    if( abs((abs(last_sample - prevTimeStamp)) - sampling_interval) < abs((abs(timestamp - last_sample)) - sampling_interval)):
+	
+    	last_sample = prevTimeStamp
+	print prevLine.strip()
+    else:
+	last_sample = timestamp
+        print line.strip()
 
