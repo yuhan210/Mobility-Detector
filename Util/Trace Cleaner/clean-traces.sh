7d858e059c4d99c4fc19949f62b22d2e1df85dfb
Tiffany Chen <yuhan210@gmail.com>
2012-11-18 17:39:32 -0500

    trace sanitizer checked in

---
+++ Util/Trace Cleaner/clean-traces.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+if [ $# -lt 2]; then
+ echo "Usage: user-trace-folder new-folder-for-clean-trace"
+ exit
+fi
+
+trace_folder=$1
+dest_folder=$2
+
+for x in "$1"* 
+do
+ echo " "
+ echo "$x"
+ python traceSanitizer.py "$x" "$dest_folder"
+
+
+done
