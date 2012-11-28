1b99d57fbaf19a6021f0ab0af911ef15e01c800c
Tiffany Chen <yuhan210@gmail.com>
2012-11-28 02:49:25 -0500

    bugs fixed for non-parametric train/classification

--- non-parametric/classifyNonParam.py
+++ non-parametric/classifyNonParam.py
@@ -14,14 +14,7 @@ class NonParamClassify(object) :
 	current_window=[]
 	last_print_out = -1
 
-	''' power stats for each phone '''
-	power_accel=dict()
-	power_wifi=dict()
-	power_gps=dict()
-	power_gsm=dict()
-	power_nwk_loc=dict()
-
-	activity_templates=dict()	
+	activity_templates=[]	
 	recs = dict()
 	def __init__(self,sim_phone) :
 		self.sim_phone=sim_phone
@@ -37,45 +30,37 @@ class NonParamClassify(object) :
 		
 		execfile("non_param_training_model",self.recs) #get activity_templates
 		self.activity_templates = self.recs['activity_templates']
-	
-	def mean_and_var(self,value_list) :
-		if (value_list==[]) :
-			return (None,None)
-		meanSq=reduce(lambda acc,update : acc + update**2,value_list,0.0)/len(value_list)
-		mean=reduce(lambda acc,update : acc + update,value_list,0.0)/len(value_list)
-		return (mean,meanSq-mean*mean)
 
-	def predict_label(self, cur_window, detection_step = 5):
+	def predict_label(self, observed_signal, detection_step = 5):
 		gamma = 1
 		# shifted detection
-		prob = dict()
+		prob = []
 
 		for i in range(5):
 			dist = []
-			prob[i] = []
+			prob[i] = 0
 			for j in range(len(self.activity_templates[i])):
-				dist.append(self.shifted_dist_func(cur_window,self.activity_templates[i][j], detection_step))
-			prob[i]+= [numpy.mean([exp(-gamma * d) for d in dist])]
+				dist.append(self.shifted_dist_func(observed_signal,self.activity_templates[i][j], detection_step))
+			prob[i] = numpy.sum([exp(-gamma * d) for d in dist])
 
 		return prob
 		
 
-	def shifted_dist_func(self,cur_window, template, detection_step):
+	def shifted_dist_func(self, cur_window, template, detection_step):
 		
 		time_interval = cur_window[-1][0] - cur_window[0][0]
 			
 		dists = []
-		for i in range(0,len(template),detection_step):
-			for j in range(i+1,len(template)):
+		for i in range(0,len(template),detection_step): # sweep through template
+			for j in range(i+1,len(template)):    # find right end point of current candidate
 				if template[j][0] - template[i][0] >= time_interval:
 					dists.append(self.dist_func(cur_window, template[i:j]))
 					break
-
+		# return best match
 		return min(dists)
 
-	def dist_func(self,cur_window,  segment):
+	def dist_func(self, cur_window,  segment):
 	
-		
 		downsampled_segment = []
 		downsampled_segment.append(segment[0])
 		
@@ -90,7 +75,8 @@ class NonParamClassify(object) :
 			
 			downsampled_segment.append(segment[minimum_time_dist_index])
 
-
+		downsampled_signal=numpy.array(map(lambda x : x[1],downsampled_segment))
+		cur_window_signal=numpy.array(map(lambda x : x[1], cur_window))
 		return sqrt(sum( [(downsampled_segment[i][1] - cur_window[i][1])** 2  for i in range(len(downsampled_segment))]))
 
 	def callback(self,sensor_reading,current_time) :
@@ -104,32 +90,8 @@ class NonParamClassify(object) :
 			start_time = self.current_window[0][0]
 
 			posterior_prob = []
-			if (current_time - self.last_print_out) >= self.WINDOW_IN_MILLI_SECONDS:
+			if (current_time - self.last_print_out) >= self.WINDOW_IN_MILLI_SECONDS/2.0 :
 				#print self.current_window
 				posterior_prob = self.predict_label(self.current_window)	
-				self.last_print_out = current_time - self.WINDOW_IN_MILLI_SECONDS/2
+				self.last_print_out = current_time
 				self.classifier_output.append((current_time, posterior_prob))
-
-	def energy_adapt (self, current_time, energy_budget, total_time, power_accel, callback_list, posterior_pmf) :
-			''' Vary sampling rate to adapt to energy constraints '''
-			self.energy_consumed += (current_time-self.last_energy_update) * power_accel[self.current_sampling_interval]
-			remaining_power=(energy_budget-self.energy_consumed)*1.0/(total_time-current_time)
-			print "Current sampling interval is ",self.current_sampling_interval," remaining power is ",remaining_power," Joules per millisecond "
-			# ramp down if required
-			if (( power_accel[self.current_sampling_interval] > remaining_power)) :
-				# pick the sampling interval with the max power which is less than remaining_power
-				# items() returns a list of (key,value) tuples. Key is the sampling_interval. value is the power
-				candidate_list = filter(lambda x : x[1] < remaining_power,power_accel.items())
-				self.current_sampling_interval = max(candidate_list, key = lambda x : x[1])[0]
-				self.sim_phone.change_accel_interval(self.current_sampling_interval)
-				return
-			# check if you can ramp up at all
-			do_i_ramp_up=reduce(lambda acc, update : acc or (posterior_pmf[update] >= 0.2), callback_list ,False); 
-			if (do_i_ramp_up) :
-				min_interval=self.current_sampling_interval
-				for candidate_interval in power_accel :
-					if ((power_accel[candidate_interval] < remaining_power) and (candidate_interval < min_interval)) :
-						min_interval=candidate_interval
-				self.current_sampling_interval=min_interval;
-				self.sim_phone.change_accel_interval(self.current_sampling_interval)
-				return
--- non-parametric/trainNonParam.py
+++ non-parametric/trainNonParam.py
@@ -6,85 +6,39 @@ from numpy.fft import *
 import numpy
 from normal import *
 import sys
-class  NonParametricTrain(object) :
+class  Train(object) :
 
 	''' Windowing primitives '''
 	last_print_out=-1
 	WINDOW_IN_MILLI_SECONDS=5000
 	current_window=[]
 	
-
 	''' Data required for ML estimates for each label '''
 	''' Key to the hash table is the label number '''
-	#mean_stats=dict()
-	#sigma_stats=dict()
-	#peak_freq_stats=dict()
-	#strength_var_stats=dict()
-
-	activity_templates =dict()
-
+	activity_templates = [[]]*5
 
 	def __init__(self,sim_phone) :
 		self.sim_phone=sim_phone
-		for i in range(5):
-			self.activity_templates[i]=[]
-
-	def mean_and_var(self,value_list) :
-		if (value_list==[]) :
-			return (None,None)
-		meanSq=reduce(lambda acc,update : acc + update**2,value_list,0.0)/len(value_list)
-		mean=reduce(lambda acc,update : acc + update,value_list,0.0)/len(value_list)
-		return (mean,meanSq-mean*mean)
-
-	def spike_normalization(self, value_list):
-		ALPHA = 1.2
-		if (value_list==[]):
-			return (None)
-		w_smoothed = []
-		for i in range(0,len(value_list)):
-			if i == 0:
-				w_smoothed.append(0)
-			else:
-				w_smoothed.append(abs(value_list[i]-valule_list[i-1])** ALPHA )
-		
-		return w_smoothed 
-
-
+	
 	def callback(self,sensor_reading,current_time,gnd_truth) :
+
 		if (isinstance(sensor_reading,Accel)) :
-			
-		
 			''' compute accel magnitude and keep track of windows '''
 			accel_mag=sqrt(sensor_reading.accel_x**2+sensor_reading.accel_y**2+sensor_reading.accel_z**2)
 		        self.current_window=filter(lambda x : x[0] >=  current_time - self.WINDOW_IN_MILLI_SECONDS * 2,self.current_window)
 		        self.current_window+=[(current_time,accel_mag)]
 			startTime = self.current_window[0][0]
-			
 			template = map(lambda x: ((x[0]-startTime),x[1]), self.current_window);
-			if len(self.activity_templates[gnd_truth]) == 0:
-				if template[-1][0] >= self.WINDOW_IN_MILLI_SECONDS * 2:
-					self.last_print_out = current_time
-					self.activity_templates[gnd_truth] += [template]
+			self.last_print_out=current_time if self.last_print_out == -1 else self.last_print_out
+			if (current_time - self.last_print_out)  >= self.WINDOW_IN_MILLI_SECONDS: 
+				self.last_print_out = current_time
+				self.activity_templates[gnd_truth] += [template]
 
-			else:
-				if (current_time - self.last_print_out)  >= self.WINDOW_IN_MILLI_SECONDS: 
-					self.last_print_out = current_time
-					self.activity_templates[gnd_truth] += [template]
-
-			
-
-			''' get spike-normalized window '''
-			#w_smoothed = self.spike_normalization(map(lambda x: x[1], self.current_window));
-				
-		
 	def output_classifer(self) :
 
 		# make sure each activity has the same number of templates
-		minimum_template_num = min(map(lambda x: len(self.activity_templates[x]), self.activity_templates))
-		
+		minimum_template_num = min(map(lambda x: len(x), self.activity_templates))
 		for i in range(5):
 			while len(self.activity_templates[i]) > minimum_template_num:
 				self.activity_templates[i].pop(0)	
-
-		
 		print "activity_templates = ",self.activity_templates
