89ccec5123655f71e638b20ba90c87f9f2b17ea3
Tiffany Chen <yuhan210@gmail.com>
2013-01-25 00:56:06 +0800

    dt java2python checked in

---
+++ Util/dt_java_2_py/java_classifier_2_python.py
@@ -0,0 +1,55 @@
+import sys
+
+def write_header():
+	print 'class DecisionTree(object):\n'
+	print '\tfv = []\n'
+	print '\tdef __init__(self,fv):'
+	print '\t\tself.fv = fv\n'
+	print '\t\tself.classify()'
+	print '\tdef classify(self):'
+
+if __name__ == "__main__":
+	if ( len(sys.argv) < 2):
+		print "Usage:", sys.argv[0],'classifier_output_from_weka'
+		exit(5)
+
+
+	write_header()
+	classifier_fhandle = open(sys.argv[1],'r')
+	
+	
+	content = classifier_fhandle.readlines()
+	condition_list = []
+	for i in xrange(len(content)):
+		if content[i].find('public static double classify') >= 0:
+			first_function_index = i
+		if content[i].find('static double N') >= 0:
+			condition_list.append(i)
+
+	first_function = content[first_function_index+4].split('.')[1].split('(')[0]
+	print '\t\t p = self.'+ first_function+ '()'
+	print '\t\t return p'
+	for index in condition_list:	
+		print '\n\tdef '+ content[index].split(' ')[4].split('(')[0]+'(self):\n'
+		feature_num =  content[index+4].split('[')[1].split(']')[0]
+		threshold = content[index+4].split('=')[1].split(' ')[1].split(')')[0]
+
+		print '\t\tif fv['+feature_num+'] <= ' + threshold+ ':'
+	
+		if content[index+5].find('yuhan.') >= 0: # call another function
+			function_name = content[index+5].split('.')[1].split('(')[0]
+			print '\t\t\tp = self.'+ function_name + '()'
+		else: # return label
+			predicted_label = content[index+5].split('=')[1].split(' ')[1].split(';')[0]
+			print '\t\t\tp = '+ predicted_label
+
+		print '\t\telif fv[' + feature_num + '] > ' + threshold + ':'
+		
+		if content[index+7].find('yuhan.') >= 0: # call another function
+			function_name = content[index+7].split('.')[1].split('(')[0]
+			print '\t\t\tp = self.'+ function_name + '()'
+		else: # return label
+			predicted_label = content[index+7].split('=')[1].split(' ')[1].split(';')[0]
+			print '\t\t\tp = '+ predicted_label
+		print '\t\treturn p'
+
