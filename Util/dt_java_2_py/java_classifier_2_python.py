f138b05c79a7d2569733dedb902727e8c87acf45
Tiffany Chen <yuhan210@gmail.com>
2013-01-31 05:48:04 +0800

    updated

--- Util/dt_java_2_py/java_classifier_2_python.py
+++ Util/dt_java_2_py/java_classifier_2_python.py
@@ -2,7 +2,7 @@ import sys
 
 def write_header():
 	print 'class DecisionTree(object):\n'
-	print '\tfv = []\n'
+	print '\tfv = {}\n'
 	print '\tdef __init__(self,fv):'
 	print '\t\tself.fv = fv\n'
 	print '\t\tself.classify()'
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 	if ( len(sys.argv) < 2):
 		print "Usage:", sys.argv[0],'classifier_output_from_weka'
 		exit(5)
-
+	feature_dict = {0:'speed', 1:'var',2:'1hz', 3:'2hz', 4:'3hz'}	
 
 	write_header()
 	classifier_fhandle = open(sys.argv[1],'r')
@@ -29,12 +29,15 @@ if __name__ == "__main__":
 	first_function = content[first_function_index+4].split('.')[1].split('(')[0]
 	print '\t\t p = self.'+ first_function+ '()'
 	print '\t\t return p'
-	for index in condition_list:	
+
+	for index in condition_list: # for each condition
 		print '\n\tdef '+ content[index].split(' ')[4].split('(')[0]+'(self):\n'
+		default_label = content[index+3].strip().split(' ')[2].split(';')[0]
+		print '\t\tp = '+ default_label
 		feature_num =  content[index+4].split('[')[1].split(']')[0]
 		threshold = content[index+4].split('=')[1].split(' ')[1].split(')')[0]
 
-		print '\t\tif fv['+feature_num+'] <= ' + threshold+ ':'
+		print '\t\tif self.fv[\''+feature_dict[int(feature_num)]+'\'] <= ' + threshold+ ':'
 	
 		if content[index+5].find('yuhan.') >= 0: # call another function
 			function_name = content[index+5].split('.')[1].split('(')[0]
@@ -43,7 +46,7 @@ if __name__ == "__main__":
 			predicted_label = content[index+5].split('=')[1].split(' ')[1].split(';')[0]
 			print '\t\t\tp = '+ predicted_label
 
-		print '\t\telif fv[' + feature_num + '] > ' + threshold + ':'
+		print '\t\telif self.fv[\'' + feature_dict[int(feature_num)] + '\'] > ' + threshold + ':'
 		
 		if content[index+7].find('yuhan.') >= 0: # call another function
 			function_name = content[index+7].split('.')[1].split('(')[0]
